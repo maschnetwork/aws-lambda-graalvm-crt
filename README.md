0618e12d76395e8bbff75b06de6c2fd2639ff1a3
Maximilian Schellhorn <mxschell@amazon.de>
2022-06-20 16:16:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# aws-lambda-graalvm-crt
-Sample AWS Lambda application with GraalVM and AWS CRT client writing to Amazon DynamoDB
+# AWS Lambda Java GraalVM function with AWS CRT client
+
+Sample AWS Lambda application with GraalVM and AWS CRT client writing to Amazon DynamoDB.
+
+## Load test results
+
+0.. warm start
+1.. cold start (init duration + duration)
+
+## Prerequisites
+
+- AWS Account
+- Docker
+- AWS SAM CLI
+- Artillery for load testing
+
+## Setup
+
+1. Create the bundling image:
+
+```bash
+docker build -t al2-graalvm:maven .
+```
+
+2. Build the application via the bundling image:
+
+```bash
+sam build --build-image al2-graalvm:maven --use-container && sam deploy
+```
+
+3. Deploy the application
+
+```bash
+sam deploy ## --guided for the first run
+```
+
+3. Test the application
+
+Get the API-Gateway Url:
+
+```bash
+ export API_GW_URL=$(aws cloudformation describe-stacks --stack-name <YOUR-STACK_NAME> | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "RequestHeaderGraalApi").OutputValue')
+```
+
+Do a sample request:
+
+```bash
+curl -XPOST "$API_GW_URL/request-headers-graal" --header 'Content-Type: application/json'
+```
+
+
+## Run a load test
+
+Run the test via:
+
+```bash
+artillery run -t $API_GW_URL  -v '{ "url": "/request-headers-graal" }' loadtest.yaml
+```
+
+Retrieve results in Cloudwatch-Log insights via:
+
+```bash
+ filter @type = "REPORT"
+    | parse @log /\d+:\/aws\/lambda\/(?<function>.*)/
+    | stats
+    count(*) as invocations,
+    pct(@duration+coalesce(@initDuration,0), 0) as p0,
+    pct(@duration+coalesce(@initDuration,0), 25) as p25,
+    pct(@duration+coalesce(@initDuration,0), 50) as p50,
+    pct(@duration+coalesce(@initDuration,0), 75) as p75,
+    pct(@duration+coalesce(@initDuration,0), 90) as p90,
+    pct(@duration+coalesce(@initDuration,0), 95) as p95,
+    pct(@duration+coalesce(@initDuration,0), 99) as p99,
+    pct(@duration+coalesce(@initDuration,0), 100) as p100
+    group by function, ispresent(@initDuration) as coldstart
+    | sort by coldstart, function
+```
---
+++ request-writer/src/main/java/com/maschnetwork/RequestHeaderWriter.java
@@ -0,0 +1,55 @@
+package com.maschnetwork;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.core.SdkSystemSetting;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RequestHeaderWriter implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final Logger logger = LoggerFactory.getLogger(RequestHeaderWriter.class);
+    private final DynamoDbAsyncClient dynamoDbClient = DynamoDbAsyncClient.builder()
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .region(Region.of(System.getenv(SdkSystemSetting.AWS_REGION.environmentVariable())))
+            .httpClientBuilder(AwsCrtAsyncHttpClient.builder())
+            .build();
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        try {
+            Map<String, AttributeValue> itemAttributes = new HashMap<>();
+            itemAttributes.put("id", AttributeValue.builder().s(context.getAwsRequestId()).build());
+            itemAttributes.put("value",
+                    AttributeValue.builder().s(
+                            input.getHeaders().entrySet()
+                                    .stream()
+                                    .map(a -> a.getKey()+"="+a.getValue())
+                                    .collect(Collectors.joining())
+                    ).build()
+            );
+
+            dynamoDbClient.putItem(PutItemRequest.builder()
+                    .tableName("request-headers")
+                    .item(itemAttributes)
+                    .build());
+            return response.withBody("successful").withStatusCode(200);
+        } catch (DynamoDbException e) {
+            logger.error("Error while executing request", e);
+            return response.withBody("error").withStatusCode(500);
+        }
+    }
+}
